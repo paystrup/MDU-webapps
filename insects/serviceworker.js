b2f4c5bcf5319a05954756b4a574397b876a7733
paystrup <90240787+paystrup@users.noreply.github.com>
2022-10-04 08:58:21 +0200

    Update serviceworker.js

--- insects/serviceworker.js
+++ insects/serviceworker.js
@@ -1,4 +1,7 @@
+// path /insects added for github pages directory
 const cacheName = 'cache-insects';
+
+// cache all insects + html in array
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
@@ -6,6 +9,9 @@ self.addEventListener('install', function(event) {
     })
   );
 });
+
+
+// fetch from cache
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(() =>
